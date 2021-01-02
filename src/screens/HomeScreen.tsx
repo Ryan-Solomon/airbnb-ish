4855ffa59caa17c7f38b34c4e38fc691c578195a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-02 16:31:22 -0600

    add bg image

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import { Image, Text, StyleSheet } from 'react-native';
+import { ImageBackground, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
 export const HomeScreen = () => {
-  return <Text>Home</Text>;
+  return (
+    <SContainer>
+      <ImageBackground
+        source={require('../../assets/images/wallpaper.jpg')}
+        style={styles.backgroundImage}
+      />
+    </SContainer>
+  );
 };
+
+// Styles
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    height: 500,
+    width: '100%',
+    resizeMode: 'cover',
+  },
+});
+
+const SContainer = styled.View``;
