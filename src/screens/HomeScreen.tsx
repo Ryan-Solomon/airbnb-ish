1f00d681d1b2f26395e673f484ab32ce4eef7b65
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-02 16:18:09 -0600

    add text styled component

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Image, Text, StyleSheet } from 'react-native';
+
+export const HomeScreen = () => {
+  return <Text>Home</Text>;
+};
---
+++ src/styles/text.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+type TextProps = {
+  fontSize: string;
+  fontColor: string;
+  margin: string;
+};
+
+const SText = styled.Text<Partial<TextProps>>`
+  font-size: ${({ fontSize }) => fontSize || '16px'};
+  color: ${({ fontColor }) => fontColor || 'white'};
+  margin: ${({ margin }) => margin || ''};
+`;
