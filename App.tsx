f99df8e8fd2e6b4b469e68ac244a4948ee63f1a5
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-07 13:52:44 -0600

    add post bp

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { HomeScreen } from './src/screens/HomeScreen';
+import { Post } from './src/components/Post';
 
 export default function App() {
-  return <HomeScreen />;
+  return <Post />;
 }
---
+++ src/components/Post.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import styled from 'styled-components/native';
+import { SText } from '../styles/text';
+
+type TProps = {
+  //
+};
+
+export const Post: FC<TProps> = ({}) => {
+  return <SText fontColor='black'>Post</SText>;
+};
