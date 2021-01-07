c1f2922f2699677bf40a103a39cbae09854ca7ed
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-07 14:28:30 -0600

    add post component

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ const img = require('./assets/images/postImg.jpg');
 const fakePost: TPost = {
   image: img,
   description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi itaque veniam recusandae totam? Non est corrupti nulla mollitia magnam, pariatur at architecto omnis voluptates adipisci dignissimos. Maiores illum similique cupiditate adipisci, debitis praesentium laudantium nobis facere consequuntur omnis dignissimos esse sequi quam, iste quas fugiat obcaecati vel quibusdam. At reprehenderit cum quos eaque. Tenetur, vero fugiat!',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad harum sapiente dicta animi odio temporibus saepe officiis soluta ratione nobis.',
   pricePerNight: '$30',
   totalPrice: '$100',
   type: '1 bed 1 bath',
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import styled from 'styled-components/native';
 import { TPost } from '../types/appTypes';
 import { Image, StyleSheet } from 'react-native';
+import { SText } from '../styles/text';
 
 type TProps = {
   post: TPost;
@@ -16,6 +17,18 @@ export const Post: FC<TProps> = ({ post }) => {
         style={styles.image}
         source={require('../../assets/images/postImg.jpg')}
       />
+      <SText margin='2px 0px' fontColor='#949494'>
+        {type}
+      </SText>
+      <SText fontSize='20px' margin='2px 0px' fontColor='#212121'>
+        {description}
+      </SText>
+      <SText margin='2px 0px' fontColor='#727272'>
+        {pricePerNight}/night
+      </SText>
+      <SText fontSize='20px' margin='2px 0px' fontColor='#383838'>
+        {totalPrice} total
+      </SText>
     </SContainer>
   );
 };
@@ -26,6 +39,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     borderRadius: 15,
+    marginBottom: 5,
   },
 });
 
