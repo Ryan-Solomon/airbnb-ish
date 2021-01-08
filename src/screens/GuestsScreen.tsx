c905bb87a04fdabc2650a77f7682c0f553cb55a7
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-08 17:05:24 -0600

    render FL of GC components

--- src/screens/GuestsScreen.tsx
+++ src/screens/GuestsScreen.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import { SText } from '../styles/text';
+import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { GuestsCounter } from '../components/GuestsCounter';
+
+const guestCategories = [
+  { title: 'Adults', description: 'Ages 13 or above' },
+  { title: 'Children', description: 'Ages 2 to 12' },
+  { title: 'Infants', description: 'Under 2' },
+];
 
 export const GuestsScreen = () => {
-  return <SText fontColor='black'>Guests</SText>;
+  return (
+    <SContainer>
+      <FlatList
+        data={guestCategories}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => {
+          const { title, description } = item;
+          return <GuestsCounter title={title} description={description} />;
+        }}
+      />
+    </SContainer>
+  );
 };
+
+const SContainer = styled.View``;
