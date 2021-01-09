9b1f540abceb9de246535b3c58f4d970ec492a04
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-08 19:44:20 -0600

    add tab navigator

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { DestinationSearch } from './../screens/DestinationSearch';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from './../screens/HomeScreen';
+
+// Stack Navigator
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -21,4 +25,26 @@ const Router = () => {
 
 type RootStackParamList = {
   DestinationSearch: undefined;
+};
+
+// Tab Navigator
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
+
+const TabNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+type RootTabParamList = {
+  Home: undefined;
 };
