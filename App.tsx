45f575e8ff4b3b47273982faa81b14dfdf85243d
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-13 17:15:37 -0600

    setup aws amp

--- App.tsx
+++ App.tsx
@@ -4,6 +4,11 @@ import 'react-native-gesture-handler';
 import { Router } from './src/navigation/Router';
 import { HomeScreen } from './src/screens/HomeScreen';
 
+import Amplify from 'aws-amplify';
+// @ts-ignore
+import config from './aws-exports';
+Amplify.configure(config);
+
 export default function App() {
   return (
     <>
