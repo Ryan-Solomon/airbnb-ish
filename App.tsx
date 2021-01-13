7944a34bdb482f50e9304fa21deb882b1e6c1d20
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-13 17:44:53 -0600

    add user login

--- App.tsx
+++ App.tsx
@@ -3,18 +3,22 @@ import styled from 'styled-components/native';
 import 'react-native-gesture-handler';
 import { Router } from './src/navigation/Router';
 import { HomeScreen } from './src/screens/HomeScreen';
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 import Amplify from 'aws-amplify';
 // @ts-ignore
-import config from './aws-exports';
+import config from './src/aws-exports';
 Amplify.configure(config);
 
-export default function App() {
+function App() {
   return (
     <>
       <Router />
     </>
   );
 }
+
+export default withAuthenticator(App);
 
 const SContainer = styled.View``;
