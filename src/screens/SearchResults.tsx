73a11c2ae7a339e41bd9b2bf93ca11d1e0ea3b82
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 14:21:15 -0600

    handle errors in search results

--- src/screens/SearchResults.tsx
+++ src/screens/SearchResults.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import { Post } from '../components/Post';
-import { feedData } from './../../assets/data/feed';
 import styled from 'styled-components/native';
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { API, graphqlOperation } from 'aws-amplify';
 import { listPosts } from '../graphql/queries';
 import { TPost } from '../types/appTypes';
 
+type TStatus = 'pending' | 'error' | 'fulfilled';
+
 export const SearchResults = () => {
   const [feedData, setFeedData] = React.useState<TPost[]>([]);
+  const [status, setStatus] = React.useState<TStatus>('pending');
 
   React.useEffect(() => {
     const getPosts = async () => {
+      setStatus('pending');
       try {
         const res = (await API.graphql(graphqlOperation(listPosts))) as any;
         const data = res.data.listPosts.items as TPost[];
         setFeedData(data);
-      } catch (e) {}
+        setStatus('fulfilled');
+      } catch (e) {
+        console.log(e.message);
+        setStatus('error');
+      }
     };
 
     getPosts();
   }, []);
+
+  if (status === 'pending') {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Loading..</Text>
+      </View>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Something went wrong</Text>
+      </View>
+    );
+  }
 
   return (
     <SContainer>
