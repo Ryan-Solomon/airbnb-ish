8c274123f0c080ab8b92d34135b81cd3bf6be848
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 14:16:59 -0600

    add posts query to search results

--- src/screens/SearchResults.tsx
+++ src/screens/SearchResults.tsx
@@ -3,8 +3,25 @@ import { Post } from '../components/Post';
 import { feedData } from './../../assets/data/feed';
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listPosts } from '../graphql/queries';
+import { TPost } from '../types/appTypes';
 
 export const SearchResults = () => {
+  const [feedData, setFeedData] = React.useState<TPost[]>([]);
+
+  React.useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const res = (await API.graphql(graphqlOperation(listPosts))) as any;
+        const data = res.data.listPosts.items as TPost[];
+        setFeedData(data);
+      } catch (e) {}
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <SContainer>
       <FlatList
