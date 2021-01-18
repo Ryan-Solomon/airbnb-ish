1173eef1efe5ee3dd68ded049a20c1b0924684a2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-18 13:47:06 -0600

    add strong typing to context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,9 +1,27 @@
-import React, { createContext, FC, ReactNode, useContext } from 'react';
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useState,
+} from 'react';
 
-const AppContext = createContext({});
+type TContext = {
+  guests: number;
+  setGuests: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const initialContext: TContext = {
+  guests: 0,
+  setGuests: () => null,
+};
+
+const AppContext = createContext(initialContext);
 
 export const ContextProvider: FC<ReactNode> = ({ children }) => {
-  const appContext = {};
+  const [guests, setGuests] = useState(0);
+
+  const appContext = { guests, setGuests };
   return (
     <AppContext.Provider value={appContext}>{children}</AppContext.Provider>
   );
