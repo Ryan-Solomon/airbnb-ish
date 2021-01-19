df0349d373aee47d814f7bff894ae455c7032969
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-19 16:45:11 -0600

    fetch single post and handle loading/error status

--- src/screens/DetailedPostScreen.tsx
+++ src/screens/DetailedPostScreen.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react';
 import { DetailedPost } from '../components/DetailedPost';
 import styled from 'styled-components/native';
+import { TPost } from '../types/appTypes';
+import { TStatus } from './SearchResults';
+import { API, graphqlOperation } from 'aws-amplify';
+import { getPost } from './../graphql/queries';
 
 type Props = {
   route: {
@@ -12,7 +16,43 @@ type Props = {
 
 export const DetailedPostScreen: FC<Props> = ({ route }) => {
   const { itemId } = route.params;
-  console.log(itemId);
+  const [feedData, setFeedData] = React.useState<TPost>();
+  const [status, setStatus] = React.useState<TStatus>('pending');
+
+  React.useEffect(() => {
+    const getPosts = async () => {
+      setStatus('pending');
+      try {
+        const res = (await API.graphql(
+          graphqlOperation(getPost, {
+            id: itemId,
+          })
+        )) as any;
+        const data = res.data.getPost as TPost;
+        setFeedData(data);
+        setStatus('fulfilled');
+      } catch (e) {
+        console.log(e.message);
+        setStatus('error');
+      }
+    };
+
+    getPosts();
+  }, []);
+
+  if (status === 'pending')
+    return (
+      <LoadingContainer>
+        <LoadingText>Loading...</LoadingText>
+      </LoadingContainer>
+    );
+  if (status === 'error')
+    return (
+      <LoadingContainer>
+        <LoadingText>Something went wrong</LoadingText>
+      </LoadingContainer>
+    );
+
   return (
     <DetailedPostContainer>
       <DetailedPost />
@@ -23,4 +63,16 @@ export const DetailedPostScreen: FC<Props> = ({ route }) => {
 const DetailedPostContainer = styled.View`
   flex: 1;
   background-color: #222;
+`;
+
+const LoadingContainer = styled.View`
+  flex: 1;
+  background-color: #222;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.Text`
+  color: #fff;
+  font-size: 20px;
 `;
