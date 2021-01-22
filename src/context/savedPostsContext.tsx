0286655e89442d48341633a7f23f851c0f7e2038
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-22 13:00:21 -0600

    start setting up post reducer

--- src/context/savedPostsContext.tsx
+++ src/context/savedPostsContext.tsx
@@ -1,10 +1,11 @@
 import React, {
   createContext,
-  useReducer,
   useContext,
   FC,
   ReactNode,
+  useReducer,
 } from 'react';
+import { TPost } from '../types/appTypes';
 
 type SavedPostsContext = {};
 
@@ -12,7 +13,24 @@ const initialContext: SavedPostsContext = {};
 
 const SavedPostsContext = createContext(initialContext);
 
+function postReducer(state, action) {
+  switch (action.type) {
+    case 'ADD_POST':
+      return {};
+    case 'REMOVE_POST':
+      return {};
+    case 'CLEAR_POSTS':
+      return {};
+    default:
+      throw new Error(`Action type ${action.type} is not supported`);
+  }
+}
+
 export const SavedPostsContextProvider: FC<ReactNode> = ({ children }) => {
+  const [posts, setPosts] = useReducer([]);
+
+  const;
+
   return (
     <SavedPostsContext.Provider value={{}}>
       {children}
